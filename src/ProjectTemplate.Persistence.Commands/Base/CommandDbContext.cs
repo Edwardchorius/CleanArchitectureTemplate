84fdb44306c44513c1fea0ae9878bc0040331917
Edward Vaklinov <edwardchorius@gmail.com>
2020-05-08 10:36:25 +0300

    Added partially implemented UoW

--- src/ProjectTemplate.Persistence.Commands/Base/CommandDbContext.cs
+++ src/ProjectTemplate.Persistence.Commands/Base/CommandDbContext.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
+using System.Threading.Tasks;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using ProjectTemplate.Persistence.Commands.Interfaces;
 
 namespace ProjectTemplate.Persistence.Commands.Base
 {
+    //TO DO -> SaveChanges
     public abstract class CommandDbContext : DbContext
     {
         private readonly IMediator _mediator;
+        private ITransaction _currentTransaction;
 
         public CommandDbContext(DbContextOptions options, IMediator mediator)
             : base(options)
@@ -16,6 +21,56 @@ namespace ProjectTemplate.Persistence.Commands.Base
             _mediator = mediator;
         }
 
-        // More work to be done
+        public bool HasActiveTransaction => _currentTransaction != null;
+
+        public async Task BeginTransactionAsync()
+        {
+            if (HasActiveTransaction)
+            {
+                throw new Exception("There is active transaction that has not finished.");
+            }
+
+            _currentTransaction = new EfTransaction(await Database.BeginTransactionAsync(IsolationLevel.ReadCommitted));
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            if (HasActiveTransaction)
+            {
+                try
+                {
+                    await _currentTransaction.RollbackAsync();
+                }
+                finally
+                {
+                    _currentTransaction.Dispose();
+                    _currentTransaction = null;
+                }
+            }
+        }
+
+        public async Task CommitAsync()
+        {
+            if (HasActiveTransaction == false)
+            {
+                throw new InvalidOperationException("Not current transaction.");
+            }
+
+            try
+            {
+                await SaveChangesAsync();
+                await _currentTransaction.CommitAsync();
+            }
+            catch
+            {
+                await _currentTransaction.RollbackAsync();
+                throw;
+            }
+            finally
+            {
+                _currentTransaction.Dispose();
+                _currentTransaction = null;
+            }
+        }
     }
 }
